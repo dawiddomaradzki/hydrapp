b987d2d5dd0bfa12b3eb7701683976f8d7581d48
Dawid Domaradzki <dawid-kun@wp.pl>
2020-05-11 20:02:23 +0200

    final version 2

--- src/js/main.js
+++ src/js/main.js
@@ -21,31 +21,31 @@ console.log(`Hello world!`);
 var glassNumber = document.querySelector('.hydrapp__wrapper--number--js');
 const addGlass = document.querySelector('.hydrapp__AddButton--js');
 const deleteGlass = document.querySelector('.hydrapp__DeleteButton');
-const key = new Date().toISOString().slice(0, 10); 
-var glass = 0;
-glassNumber.innerHTML = localStorage.getItem(key);
+const key = new Date().toISOString().slice(0, 10);
 
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  glassNumber.innerHTML = '0';
+} else { 
+  glassNumber.innerHTML = localStorage.getItem(key); }
 
 addGlass.addEventListener('click', (e) => {
-  e.preventDefault();
-  const adding = Number(glassNumber.innerHTML) + 1;
+  const adding = parseInt(glassNumber.innerHTML) + 1;
   glassNumber.innerHTML = adding;
   console.log(`${adding} szklanka wody`);
   localStorage.setItem(key, adding);
-
 });
 
 deleteGlass.addEventListener('click', (e) => {
-  e.preventDefault();
-  const reduction = Number(glassNumber.innerHTML) - 1;
+  const reduction = parseInt(glassNumber.innerHTML) - 1;
   if (reduction >= 0) {
     glassNumber.innerHTML = reduction;
     console.log(`${reduction} szklanka wody`);
     localStorage.setItem(key, reduction);
-
   }
 
   if (reduction < 0) {
     glassNumber.innerHTML = 0;
-    console.log(`nie możesz wypić mniej niż 0 szklanek`);  }
+    console.log(`nie możesz wypić mniej niż 0 szklanek`);
+  }
 });
